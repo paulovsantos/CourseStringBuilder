0880f62f406ae5e3255f4a94aeec82c96abf0014
paulovsantos <paulo.vitors@hotmail.com>
2022-06-27 14:47:05 -0300

    Adicionando projeto string builder

---
+++ CourseStringBuilder/Entities/Comment.cs
@@ -0,0 +1,27 @@
+﻿
+
+using System;
+
+namespace CourseStringBuilder.Entities
+{
+    class Comment
+    {
+        public string Text { get; set; }
+
+        public Comment()
+        {
+
+        }
+        public Comment(string text)
+        {
+            Text = text;
+        }
+
+        internal void Remove(Comment comment)
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+  
+}
---
+++ CourseStringBuilder/Entities/Post.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace CourseStringBuilder.Entities
+{
+    class Post
+    {
+        public DateTime Moment { get; set; }
+        public string Title { get; set; }
+        public string Content { get; set; }
+        public int Likes { get; set; }
+        public  Comment Comment { get; set; }
+        public List<Comment> Comments { get; set; } = new List<Comment>();
+
+        public void AddComment(Comment comment)
+        {
+            Comments.Add(comment);
+        }
+
+        public void RemoveComment(Comment comment)
+        {
+            Comment.Remove(comment);
+        }
+
+    }
+}

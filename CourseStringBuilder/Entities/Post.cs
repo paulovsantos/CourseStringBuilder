e8490002942f0b8513945a91e679b9a6a49b5b8e
paulovsantos <paulo.vitors@hotmail.com>
2022-06-27 16:00:07 -0300

    Projeto finalizado

--- CourseStringBuilder/Entities/Post.cs
+++ CourseStringBuilder/Entities/Post.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Text;
 
 namespace CourseStringBuilder.Entities
 {
@@ -9,8 +10,21 @@ namespace CourseStringBuilder.Entities
         public string Title { get; set; }
         public string Content { get; set; }
         public int Likes { get; set; }
-        public  Comment Comment { get; set; }
+        
         public List<Comment> Comments { get; set; } = new List<Comment>();
+
+        public Post()
+        {
+
+        }
+
+        public Post(DateTime moment, string title, string content, int likes)
+        {
+            Moment = moment;
+            Title = title;
+            Content = content;
+            Likes = likes;
+        }
 
         public void AddComment(Comment comment)
         {
@@ -19,7 +33,22 @@ namespace CourseStringBuilder.Entities
 
         public void RemoveComment(Comment comment)
         {
-            Comment.Remove(comment);
+            Comments.Remove(comment);
+        }
+
+        public override string ToString()
+        {
+            StringBuilder sb = new StringBuilder();
+            sb.AppendLine(Title);
+            sb.Append(Likes);
+            sb.Append(" Likes - ");
+            sb.AppendLine(Moment.ToString("dd/MM/yyyy HH:mm:ss"));
+            sb.AppendLine(Content);
+            foreach(Comment c in Comments)
+            {
+                sb.AppendLine(c.Text);
+            }
+            return sb.ToString();
         }
 
     }
